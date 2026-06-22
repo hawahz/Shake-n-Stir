ea6ece24fbff740dd47d9433f82bf8baa9bc4bdd
hawah <xmzh233@163.com>
2026-06-22 20:50:21 +0800

    fix(gui): 修复HUD显示逻辑
    - 添加了Minecraft客户端实例导入
    - 添加了静态导入MC工具类
    - 增加了隐藏GUI选项和观察者模式检查
    - 在ShakeContentHud中添加了getPlayer静态导入
    - 修复了isVisible方法中的条件判断逻辑

--- src/main/java/io/github/hawah/shakenstir/client/gui/AbstractBlockTargetHUD.java
+++ src/main/java/io/github/hawah/shakenstir/client/gui/AbstractBlockTargetHUD.java
@@ -1,15 +1,21 @@
 package io.github.hawah.shakenstir.client.gui;
 
 import io.github.hawah.shakenstir.client.ClientDataHolder;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.HitResult;
 import net.neoforged.neoforge.client.gui.GuiLayer;
 
 import javax.annotation.Nonnull;
 
+import static io.github.hawah.shakenstir.client.gui.MC.*;
+
 public abstract class AbstractBlockTargetHUD implements GuiLayer {
     protected boolean isVisible() {
         if (ClientDataHolder.Picker.type().equals(HitResult.Type.MISS)) {
+            return false;
+        }
+        if (Minecraft.getInstance().options.hideGui || ( getPlayer() != null && getPlayer().isSpectator())) {
             return false;
         }
         return block().equals(ClientDataHolder.Picker.block().orElse(null));
